ee28f040e4da697457d1cc9390094714bf695a81
Aidan Hawthorne <ahawthorne00@gmail.com>
2023-12-14 10:28:51 -0500

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import reactor.core.publisher.Mono;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        DiscordClient client = DiscordClient.create("");
+
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> {
+            // ReadyEvent example
+            Mono<Void> printOnLogin = gateway.on(ReadyEvent.class, event ->
+                            Mono.fromRunnable(() -> {
+                                final User self = event.getSelf();
+                                System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
+                            }))
+                    .then();
+
+            // MessageCreateEvent example
+            Mono<Void> handlePingCommand = gateway.on(MessageCreateEvent.class, event -> {
+                Message message = event.getMessage();
+
+                if (message.getContent().equalsIgnoreCase("!ping")) {
+                    return message.getChannel()
+                            .flatMap(channel -> channel.createMessage("pong"));
+                }
+
+                return Mono.empty();
+            }).then();
+
+            // combine them!
+            return printOnLogin.and(handlePingCommand);
+        });
+
+        login.block();
+
+
+    }
+}
